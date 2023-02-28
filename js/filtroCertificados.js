7d9b80e7da0ab8d3e562c2bfb045553bd83ce1ed
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2023-02-28 18:07:12 -0300

    Add files via upload
    finalizado parte certificado

--- js/filtroCertificados.js
+++ js/filtroCertificados.js
@@ -1,7 +1,31 @@
-var campoFiltro = document.querySelector("#certificados")
+var campoFiltro = document.querySelector("#certificados");
 
-campoFiltro.addEventListener('input', function() {
+campoFiltro.addEventListener('input', function () {
 
-    
+    var certificados = document.querySelectorAll('#cert');
+
+    if (this.value.length > 0) {
+
+        for (i = 0; i < certificados.length; i++) {
+
+            var cert = certificados[i];
+            var nome = cert.textContent;
+
+            var expressao = new RegExp(this.value,"i");
+
+            if(!expressao.test(nome)) {
+                cert.classList.add("invisivel__cert")
+            } else {
+                cert.classList.remove("invisivel__cert")
+            };
+
+        }
+
+    } else {
+        for(i = 0; i < certificados.length; i++) {
+            var cert = certificados[i];
+            cert.classList.remove("invisivel__cert")
+        }
+    }
 
 });
